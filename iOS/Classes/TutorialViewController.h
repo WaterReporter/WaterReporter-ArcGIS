717f17d0bad46774d9bc60e8eb5bf0f5225946dd
joshua+github@developedsimple.com <joshua@developedsimple.com>
2013-03-27 11:08:03 -0400

    Moved WR to iOS

---
+++ iOS/Classes/TutorialViewController.h
@@ -0,0 +1,16 @@
+//
+//  TutorialViewController.h
+//  WaterReporter
+//
+//  Created by Viable Industries on 3/25/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TutorialViewController : UIViewController {
+    
+}
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
+@end
